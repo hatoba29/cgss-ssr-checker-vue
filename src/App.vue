b7507787f9384413635f482d14cbaaedb5ee363e
hatoba29 <hatoba29@naver.com>
2022-04-03 00:13:00 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Hello, Vue!</h1>
+</template>
+
+<style lang="scss">
+@import "./assets/base.css";
+@import "./assets/normalize.css";
+</style>
