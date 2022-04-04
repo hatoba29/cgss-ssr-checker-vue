7d4c6c66b1ddc9e62ce0a2fa4ef6614f61db3fbb
hatoba29 <hatoba29@naver.com>
2022-04-04 12:10:02 +0900

    Fix: remove unnecessary action in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import { reactive } from "vue"
 import type { Option } from "@/types/option"
 
-// State
 const options: Option = reactive({
   cardName: "",
   idolName: "",
@@ -10,19 +9,5 @@ const options: Option = reactive({
   showName: true,
   awaken: true,
 })
-
-// Action
-export const setType = (key: keyof Option["type"], value: boolean) => {
-  options.type[key] = value
-}
-export const setLimited = (key: keyof Option["limited"], value: boolean) => {
-  options.limited[key] = value
-}
-export const setName = (key: "cardName" | "idolName", value: string) => {
-  options[key] = value
-}
-export const setBool = (key: "showName" | "awaken", value: boolean) => {
-  options[key] = value
-}
 
 export default options
